731a9ec2eb998976949a13eefe48818f4833f901
xixiaoyu <xixiaoyu@yundianjia.com>
2016-10-20 23:27:59 +0800

    add static_pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "welcome ruby on rails 5"
   end
 
 end
